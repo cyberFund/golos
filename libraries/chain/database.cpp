0358cb41dd71646f5d8cb8e8288d6215bf318f43
ValeryLitvin <litvin.besson@gmail.com>
2016-10-17 18:52:08 +0200

    Updated for testnetPR4

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -2216,7 +2216,7 @@ asset database::get_pow_reward()const
 #endif
 
    static_assert( STEEMIT_BLOCK_INTERVAL == 3, "this code assumes a 3-second time interval" );
-   static_assert( STEEMIT_MAX_MINERS == 21, "this code assumes 21 per round" );
+   // static_assert( STEEMIT_MAX_MINERS == 21, "this code assumes 21 per round" );
    asset percent( calc_percent_reward_per_round< STEEMIT_POW_APR_PERCENT >( props.virtual_supply.amount ), STEEM_SYMBOL);
    return std::max( percent, STEEMIT_MIN_POW_REWARD );
 }
