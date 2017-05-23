67cfba205fe8f376a5275dc1bb38d692a08cc2bd
Mikhail Komarov <nemo1369@gmail.com>
2017-05-23 13:47:50 +0300

    Initial comment editing changes #85

--- libraries/chain/steem_evaluator.cpp
+++ libraries/chain/steem_evaluator.cpp
@@ -557,12 +557,19 @@ namespace steemit {
                 {
                     const auto &comment = *itr;
 
-                    if (_db.has_hardfork(STEEMIT_HARDFORK_0_14__306))
-                        FC_ASSERT(comment.mode !=
-                                  archived, "The comment is archived.");
-                    else if (_db.has_hardfork(STEEMIT_HARDFORK_0_10))
-                        FC_ASSERT(comment.last_payout ==
-                                  fc::time_point_sec::min(), "Can only edit during the first 24 hours.");
+                    if( _db.has_hardfork( STEEMIT_HARDFORK_0_17__85 ) )
+      {
+         // This will be moved to the witness plugin in a later release
+         if( _db.is_producing() )
+         {
+            // For now, use the same editting rules, but implement it as a soft fork.
+            FC_ASSERT( comment.mode != archived, "The comment is archived." );
+         }
+      }
+      else if( _db.has_hardfork( STEEMIT_HARDFORK_0_14__306 ) )
+         FC_ASSERT( comment.mode != archived, "The comment is archived." );
+      else if( _db.has_hardfork( STEEMIT_HARDFORK_0_10 ) )
+         FC_ASSERT( comment.last_payout == fc::time_point_sec::min(), "Can only edit during the first 24 hours." );
 
                     _db.modify(comment, [&](comment_object &com) {
                         com.last_update = _db.head_block_time();
