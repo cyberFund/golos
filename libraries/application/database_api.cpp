7580075e903a8bf69e9f0f65a34ff8ba0160f788
Mikhail Komarov <nemo1369@gmail.com>
2017-06-01 23:59:30 +0300

    Formatting changes

--- libraries/application/database_api.cpp
+++ libraries/application/database_api.cpp
@@ -2751,15 +2751,13 @@ namespace steemit {
             });
         }
 
-        reward_fund_api_obj database_api::get_reward_fund( string name )const
-{
-   return my->_db.with_read_lock( [&]()
-   {
-      auto fund = my->_db.find< reward_fund_object, by_name >( name );
-      FC_ASSERT( fund != nullptr, "Invalid reward fund name" );
+        reward_fund_api_obj database_api::get_reward_fund(string name) const {
+            return my->_db.with_read_lock([&]() {
+                auto fund = my->_db.find<reward_fund_object, by_name>(name);
+                FC_ASSERT(fund != nullptr, "Invalid reward fund name");
 
-      return *fund;
-   });
-}
+                return *fund;
+            });
+        }
     }
 } // steemit::app
