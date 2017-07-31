48fd41525b20745bf6d3334ba09dd7207f1bdef2
Mikhail Komarov <nemo1369@gmail.com>
2017-08-01 02:29:54 +0300

    Redundant literals usage removed. Correct json_metadata unpacking is not required now

--- libraries/plugins/tags/tags_plugin.cpp
+++ libraries/plugins/tags/tags_plugin.cpp
@@ -109,11 +109,10 @@ namespace steemit {
                 comment_metadata filter_tags(const comment_object &c) const {
                     comment_metadata meta;
 
-                    if (!c.json_metadata.empty() && c.json_metadata != R"(\"\")") {
+                    if (!c.json_metadata.empty()) {
                         try {
                             meta = fc::json::from_string(to_string(c.json_metadata)).as<comment_metadata>();
-                        }
-                        catch (const fc::exception &e) {
+                        } catch (const fc::exception &e) {
                             // Do nothing on malformed json_metadata
                         }
                     }
@@ -473,7 +472,7 @@ namespace steemit {
 
             tags::comment_metadata meta;
 
-            if (!c.json_metadata.empty() && c.json_metadata != R"(\"\")") {
+            if (!c.json_metadata.empty()) {
                 try {
                     meta = fc::json::from_string(c.json_metadata).as<tags::comment_metadata>();
                 } catch (const fc::exception &e) {
