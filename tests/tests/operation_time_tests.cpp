5db9e0e1ba2c129d62f79d46a114ae832bd0b22d
Mikhail Komarov <nemo1369@gmail.com>
2017-08-29 01:24:18 +0300

    Operation time tests fixes

--- tests/tests/operation_time_tests.cpp
+++ tests/tests/operation_time_tests.cpp
@@ -1337,7 +1337,7 @@ BOOST_AUTO_TEST_CASE( nested_comments )
 
             // Upgrade accounts to witnesses
             for (int i = 0; i < 7; i++) {
-                transfer(STEEMIT_INIT_MINER_NAME, accounts[i], 10000);
+                transfer(STEEMIT_INIT_MINER_NAME, accounts[i], asset(10000, STEEM_SYMBOL));
                 witness_create(accounts[i], keys[i], "foo.bar", keys[i].get_public_key(), 1000);
 
                 ops.push_back(feed_publish_operation());
