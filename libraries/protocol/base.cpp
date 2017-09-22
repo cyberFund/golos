c4209e10beb7b8b414b1558ad1f9d8436fafb8aa
Mikhail Komarov <nemo1369@gmail.com>
2017-09-22 19:22:42 +0300

    Base operation usage fixes #177

--- libraries/protocol/base.cpp
+++ libraries/protocol/base.cpp
@@ -2,7 +2,8 @@
 
 namespace steemit {
     namespace protocol {
-        uint64_t base_operation::calculate_data_fee(uint64_t bytes, uint64_t price_per_kbyte) {
+        template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+        uint64_t base_operation<Major, Hardfork, Release>::calculate_data_fee(uint64_t bytes, uint64_t price_per_kbyte) {
             auto result = (fc::uint128(bytes) * price_per_kbyte) / 1024;
             FC_ASSERT(result <= STEEMIT_MAX_SHARE_SUPPLY);
             return result.to_uint64();
