d2a14f1a7e0eff3c06bd565478489b500e7fd30b
Mikhail Komarov <nemo1369@gmail.com>
2017-10-02 21:19:35 +0300

    Snapshot state reflection fixed #177

--- libraries/plugins/snapshot/include/steemit/snapshot/snapshot_state.hpp
+++ libraries/plugins/snapshot/include/steemit/snapshot/snapshot_state.hpp
@@ -51,8 +51,8 @@ namespace steemit {
     }
 }
 
-FC_REFLECT(steemit::plugin::snapshot::account_keys, (owner_key)(active_key)(posting_key)(memo_key))
-FC_REFLECT(steemit::plugin::snapshot::account_balances, (assets))
-FC_REFLECT(steemit::plugin::snapshot::snapshot_summary, (balance)(sbd_balance)(total_vesting_shares)(total_vesting_fund_steem)(accounts_count))
-FC_REFLECT(steemit::plugin::snapshot::account_summary, (id)(name)(posting_rewards)(curation_rewards)(keys)(balances)(json_metadata)(proxy)(post_count)(recovery_account)(reputation))
-FC_REFLECT(steemit::plugin::snapshot::snapshot_state, (timestamp)(head_block_num)(head_block_id)(chain_id)(summary)(accounts))
+FC_REFLECT((steemit::plugin::snapshot::account_keys), (owner_key)(active_key)(posting_key)(memo_key))
+FC_REFLECT((steemit::plugin::snapshot::account_balances), (assets))
+FC_REFLECT((steemit::plugin::snapshot::snapshot_summary), (balance)(sbd_balance)(total_vesting_shares)(total_vesting_fund_steem)(accounts_count))
+FC_REFLECT((steemit::plugin::snapshot::account_summary), (id)(name)(posting_rewards)(curation_rewards)(keys)(balances)(json_metadata)(proxy)(post_count)(recovery_account)(reputation))
+FC_REFLECT((steemit::plugin::snapshot::snapshot_state), (timestamp)(head_block_num)(head_block_id)(chain_id)(summary)(accounts))
