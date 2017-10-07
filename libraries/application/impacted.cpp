9a90a941e69c24eecd8987df0d6f0fbd96472f46
Mikhail Komarov <nemo1369@gmail.com>
2017-10-07 20:02:37 +0300

    Impacted operations usage in application usage fixes #177

--- libraries/application/impacted.cpp
+++ libraries/application/impacted.cpp
@@ -18,63 +18,76 @@ namespace steemit {
             typedef void result_type;
 
             template<typename T>
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
             void operator()(const T &op) {
                 op.get_required_posting_authorities(_impacted);
                 op.get_required_active_authorities(_impacted);
                 op.get_required_owner_authorities(_impacted);
             }
 
-            void operator()(const account_create_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const account_create_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.new_account_name);
                 _impacted.insert(op.creator);
             }
 
-            void operator()(const account_update_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const account_update_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account);
             }
 
-            void operator()(const comment_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const comment_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.author);
                 if (op.parent_author.size()) {
                     _impacted.insert(op.parent_author);
                 }
             }
 
-            void operator()(const delete_comment_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const delete_comment_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.author);
             }
 
-            void operator()(const vote_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const vote_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.voter);
                 _impacted.insert(op.author);
             }
 
-            void operator()(const author_reward_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const author_reward_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.author);
             }
 
-            void operator()(const curation_reward_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const curation_reward_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.curator);
             }
 
-            void operator()(const liquidity_reward_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const liquidity_reward_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const interest_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const interest_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const fill_convert_request_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const fill_convert_request_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const transfer_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const transfer_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
             }
 
-            void operator()(const transfer_to_vesting_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const transfer_to_vesting_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
 
                 if (op.to != account_name_type() && op.to != op.from) {
@@ -82,59 +95,72 @@ namespace steemit {
                 }
             }
 
-            void operator()(const withdraw_vesting_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const withdraw_vesting_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account);
             }
 
-            void operator()(const witness_update_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const witness_update_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const account_witness_vote_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const account_witness_vote_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account);
                 _impacted.insert(op.witness);
             }
 
-            void operator()(const account_witness_proxy_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const account_witness_proxy_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account);
                 _impacted.insert(op.proxy);
             }
 
-            void operator()(const feed_publish_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const feed_publish_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.publisher);
             }
 
-            void operator()(const limit_order_create_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const limit_order_create_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const fill_order_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const fill_order_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.current_owner);
                 _impacted.insert(op.open_owner);
             }
 
-            void operator()(const fill_call_order_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const fill_call_order_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const fill_settlement_order_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const fill_settlement_order_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const limit_order_cancel_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const limit_order_cancel_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
-            void operator()(const pow_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const pow_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.worker_account);
             }
 
-            void operator()(const fill_vesting_withdraw_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const fill_vesting_withdraw_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from_account);
                 _impacted.insert(op.to_account);
             }
 
-            void operator()(const shutdown_witness_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const shutdown_witness_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.owner);
             }
 
@@ -144,98 +170,118 @@ namespace steemit {
                 }
             }
 
-            void operator()(const request_account_recovery_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const request_account_recovery_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account_to_recover);
             }
 
-            void operator()(const recover_account_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const recover_account_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account_to_recover);
             }
 
-            void operator()(const change_recovery_account_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const change_recovery_account_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account_to_recover);
             }
 
-            void operator()(const escrow_transfer_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const escrow_transfer_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
                 _impacted.insert(op.agent);
             }
 
-            void operator()(const escrow_approve_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const escrow_approve_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
                 _impacted.insert(op.agent);
             }
 
-            void operator()(const escrow_dispute_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const escrow_dispute_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
                 _impacted.insert(op.agent);
             }
 
-            void operator()(const escrow_release_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const escrow_release_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
                 _impacted.insert(op.agent);
             }
 
-            void operator()(const transfer_to_savings_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const transfer_to_savings_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
             }
 
-            void operator()(const transfer_from_savings_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const transfer_from_savings_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
                 _impacted.insert(op.to);
             }
 
-            void operator()(const cancel_transfer_from_savings_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const cancel_transfer_from_savings_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.from);
             }
 
-            void operator()(const decline_voting_rights_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const decline_voting_rights_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account);
             }
 
-            void operator()(const comment_benefactor_reward_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const comment_benefactor_reward_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.benefactor);
                 _impacted.insert(op.author);
             }
 
-            void operator()(const delegate_vesting_shares_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const delegate_vesting_shares_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.delegator);
                 _impacted.insert(op.delegatee);
             }
 
-            void operator()(const return_vesting_delegation_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const return_vesting_delegation_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.account);
             }
 
-            void operator()(const asset_update_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const asset_update_operation<Major, Hardfork, Release> &op) {
                 if (op.new_issuer) {
                     _impacted.insert(*(op.new_issuer));
                 }
             }
 
-            void operator()(const asset_issue_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const asset_issue_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.issue_to_account);
             }
 
-            void operator()(const override_transfer_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const override_transfer_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.to);
                 _impacted.insert(op.from);
                 _impacted.insert(op.issuer);
             }
 
-            void operator()(const call_order_update_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const call_order_update_operation<Major, Hardfork, Release> &op) {
             }
 
-            void operator()(const bid_collateral_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const bid_collateral_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.bidder);
             }
 
-            void operator()(const execute_bid_operation &op) {
+            template<uint8_t Major, uint8_t Hardfork, uint16_t Release>
+            void operator()(const execute_bid_operation<Major, Hardfork, Release> &op) {
                 _impacted.insert(op.bidder);
             }
 
