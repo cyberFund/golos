79c300fa6f2cb341cc803a62498195aa783fe4ed
Mikhail Komarov <nemo1369@gmail.com>
2017-10-09 20:38:14 +0300

    Chain tests fixes #177

--- libraries/protocol/include/steemit/protocol/exceptions.hpp
+++ libraries/protocol/include/steemit/protocol/exceptions.hpp
@@ -16,26 +16,27 @@ namespace steemit {
             namespace transaction {
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
-                template<uint32_t Code = 3000000, typename What = boost::mpl::string<'transaction exception'>>
-                using basic = fc::basic_exception<Code, What>;
+                template<uint32_t Code = 3000000,
+                        typename What = boost::mpl::string<'transaction exception'>> using basic = fc::basic_exception<
+                        Code, What>;
 
-                template<uint32_t Code = 3010000, typename What = boost::mpl::string<'missing required active authority'>>
-                using tx_missing_active_auth = basic<Code, What>;
+                template<uint32_t Code = 3010000, typename What = boost::mpl::string<
+                        'missing required active authority'>> using tx_missing_active_auth = basic<Code, What>;
 
-                template<uint32_t Code = 3020000, typename What = boost::mpl::string<'missing required owner authority'>>
-                using tx_missing_owner_auth = basic<Code, What>;
+                template<uint32_t Code = 3020000, typename What = boost::mpl::string<
+                        'missing required owner authority'>> using tx_missing_owner_auth = basic<Code, What>;
 
-                template<uint32_t Code = 3030000, typename What = boost::mpl::string<'missing required posting authority'>>
-                using tx_missing_posting_auth = basic<Code, What>;
+                template<uint32_t Code = 3030000, typename What = boost::mpl::string<
+                        'missing required posting authority'>> using tx_missing_posting_auth = basic<Code, What>;
 
-                template<uint32_t Code = 3040000, typename What = boost::mpl::string<'missing required other authority'>>
-                using tx_missing_other_auth = basic<Code, What>;
+                template<uint32_t Code = 3040000, typename What = boost::mpl::string<
+                        'missing required other authority'>> using tx_missing_other_auth = basic<Code, What>;
 
-                template<uint32_t Code = 3050000, typename What = boost::mpl::string<'irrelevant signature included'>>
-                using tx_irrelevant_sig = basic<Code, What>;
+                template<uint32_t Code = 3050000, typename What = boost::mpl::string<
+                        'irrelevant signature included'>> using tx_irrelevant_sig = basic<Code, What>;
 
-                template<uint32_t Code = 3060000, typename What = boost::mpl::string<'duplicate signature includedty'>>
-                using tx_duplicate_sig = basic<Code, What>;
+                template<uint32_t Code = 3060000, typename What = boost::mpl::string<
+                        'duplicate signature includedty'>> using tx_duplicate_sig = basic<Code, What>;
 #pragma GCC diagnostic pop
             }
         }
