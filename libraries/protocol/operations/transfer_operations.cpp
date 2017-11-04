1cf9c18d176951e7cc29d781db7ab1b7c991da24
Mikhail Komarov <nemo1369@gmail.com>
2017-11-04 14:47:59 +0300

    Transfer operation static validation fixes

--- libraries/protocol/operations/transfer_operations.cpp
+++ libraries/protocol/operations/transfer_operations.cpp
@@ -22,7 +22,10 @@ namespace golos {
             try {
                 validate_account_name(from);
                 validate_account_name(to);
-                FC_ASSERT(amount.symbol_type_value() != VESTS_SYMBOL, "transferring of Golos Power (STMP) is not allowed.");
+                if (amount.symbol_name().size() <= 6) {
+                    FC_ASSERT(amount.symbol_type_value() != VESTS_SYMBOL,
+                              "transferring of Golos Power is not allowed.");
+                }
                 FC_ASSERT(amount.amount > 0, "Cannot transfer a negative amount (aka: stealing)");
                 FC_ASSERT(memo.size() < STEEMIT_MAX_MEMO_SIZE, "Memo is too large");
                 FC_ASSERT(fc::is_utf8(memo), "Memo is not UTF8");
