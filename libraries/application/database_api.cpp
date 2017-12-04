ca30e748da79da4e28065dc61eb9181e37a4daf1
Mikhail Komarov <nemo1369@gmail.com>
2017-12-04 23:01:56 +0300

    Database API get_delegated_vestings fixes #307

--- libraries/application/database_api.cpp
+++ libraries/application/database_api.cpp
@@ -2282,7 +2282,7 @@ namespace golos {
                 const auto &delegation_idx = my->_db.get_index<vesting_delegation_index, by_id>();
                 for (const vesting_delegation_index::value_type &itr = delegation_idx.begin();
                      result.size() < limit && itr != delegation_idx.end(); ++itr) {
-                    if (itr->delegator == account) {
+                    if (itr->delegatee == account) {
                         result.push_back(*itr);
                     }
                 }
