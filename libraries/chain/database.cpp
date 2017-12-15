e8bf32d208797e55899b4c2275c2e4b978cf383c
Mikhail Komarov <nemo1369@gmail.com>
2017-12-15 04:29:22 +0300

    Nameless constant factored out to config.hpp #309

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -2036,8 +2036,8 @@ namespace golos {
         }
 
         asset<0, 17, 0> database::get_name_cost(const fc::fixed_string<> &name) const {
-            return {get_producer_reward().amount * STEEMIT_BLOCKS_PER_DAY * 90 /
-                    std::pow(name.size() - STEEMIT_MIN_ASSET_SYMBOL_LENGTH + 1, 3), STEEM_SYMBOL_NAME};
+            return to_sbd({get_producer_reward().amount * STEEMIT_BLOCKS_PER_DAY * STEEMIT_DEFAULT_PRODUCER_REWARD_DURATION /
+                     std::pow(name.size() - STEEMIT_MIN_ASSET_SYMBOL_LENGTH + 1, 3), STEEM_SYMBOL_NAME});
         }
 
         void database::pay_liquidity_reward() {
