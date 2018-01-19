fba4576b428c7a9d77ebc840d9e45ba2c614f4ad
Mikhail Komarov <nemo1369@gmail.com>
2018-01-19 15:21:15 +0300

    Immutable chain parameters changes

--- libraries/chain/include/golos/chain/immutable_chain_parameters.hpp
+++ libraries/chain/include/golos/chain/immutable_chain_parameters.hpp
@@ -13,10 +13,10 @@ namespace golos {
          * @brief This represents non-mutable compile-time chain parameters
          */
         struct immutable_chain_parameters {
-            uint16_t min_committee_member_count = STEEMIT_DEFAULT_MIN_COMMITTEE_MEMBER_COUNT;
-            uint16_t min_witness_count = STEEMIT_DEFAULT_MIN_WITNESS_COUNT;
-            uint32_t num_special_accounts = 0;
-            uint32_t num_special_assets = 0;
+            uint16_t min_committee_member_count = STEEMIT_DEFAULT_MIN_COMMITTEE_MEMBER_COUNT; ///< Default minimum available commitee member count
+            uint16_t min_witness_count = STEEMIT_DEFAULT_MIN_WITNESS_COUNT; ///< Default minimum witness amount
+            uint32_t num_special_accounts = 0; ///< Indicates special (genesis-created, hardcoded) accounts
+            uint32_t num_special_assets = 0; ///< Indicates special (genesis-created, hardcoded) assets
         };
 
     }
